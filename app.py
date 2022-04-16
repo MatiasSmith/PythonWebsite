23777ae11d5e8d66bd4d3e77269c77010911a04e
Matias Smith <malsmith@ucdavis.edu>
2022-04-15 22:31:04 -0700

    Added logout page and comments

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 #secret key: way that we decrypt and encrypt data
 app.secret_key = "bobross"
 
+#Page path ~/
 @app.route("/")
 def home():
     return render_template("index.html", content=3)
 
+#Page path ~/login
 @app.route("/login", methods=["POST", "GET"])
 def login():
     #If we press the submit button
@@ -22,12 +24,16 @@ def login():
         #return redirect(url_for("user", usr=user, usr2=user2))
         return redirect(url_for("user"))
     else:
+        #If user has already logged in and is in session
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template("login.html")
 
 #@app.route("/<usr>/<usr2>")
 #def user(usr, usr2):
 #    return f"<h1>{usr}</h1><h2>{usr2}</h2>"
 
+#Page path ~/user
 @app.route("/user")
 def user():
     #Check if the user was in session
@@ -39,6 +45,11 @@ def user():
     #Else if theres no user in my session
     else:
         return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     app.run(debug=True)
